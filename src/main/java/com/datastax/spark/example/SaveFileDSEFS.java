9d875ac89e37f472e9e42a6039a6b257f18737ff
PatrickCallaghan <patcho@gmail.com>
2017-11-29 12:34:25 +0000

    updated for file paths

--- src/main/java/com/datastax/spark/example/SaveFileDSEFS.java
+++ src/main/java/com/datastax/spark/example/SaveFileDSEFS.java
@@ -30,10 +30,10 @@ public final class SaveFileDSEFS {
 		FileSystem fs = FileSystem.get(configuration);
 
 		//Create output stream to HDFS file
-		Path dst = new Path("dsefs://127.0.0.1:5598/patrick/tmp/NetCat.java");
+		Path dst = new Path("dsefs://127.0.0.1:5598/tmp/NetCat.java");
 
 		//Create input stream from local file
-		Path src = new Path("file:///Users//patrickcallaghan/tmp/NetCat.java");
+		Path src = new Path("file:///tmp/NetCat.java");
 
 		fs.copyFromLocalFile(src, dst);
 	}
